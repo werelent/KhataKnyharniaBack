9894eddcae41fff694f27786bed6d24ae4fd5498
werelent <83115200+werelent@users.noreply.github.com>
2023-06-14 22:02:40 +0300

    set up identity

--- PracticeWebApp/Models/DataContext.cs
+++ PracticeWebApp/Models/DataContext.cs
@@ -3,12 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace PracticeWebApp.Models
 {
-    public class DbContext : IdentityDbContext<User>
+    public class DataContext : IdentityDbContext<User>
     {
         public DbSet<Book> Books { get; set; }
         public DbSet<Order> Orders { get; set; }
 
-        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
     }
--- PracticeWebApp/Program.cs
+++ PracticeWebApp/Program.cs
@@ -1,7 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PracticeWebApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = false;
+
+    // Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings
+    options.User.RequireUniqueEmail = true;
+});
 
 var app = builder.Build();
 
@@ -18,6 +48,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
