30606b9089c7f60f81fd77bd43d1881b3268ddbf
werelent <83115200+werelent@users.noreply.github.com>
2023-06-16 15:02:07 +0300

    added swagger & deleted useless stuff

--- PracticeWebApp/Program.cs
+++ PracticeWebApp/Program.cs
@@ -7,13 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
 
 builder.Services.AddIdentity<User, IdentityRole>()
     .AddEntityFrameworkStores<DataContext>()
     .AddDefaultTokenProviders();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.Configure<IdentityOptions>(options =>
 {
@@ -36,11 +39,13 @@ builder.Services.Configure<IdentityOptions>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -50,7 +55,5 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapRazorPages();
 
 app.Run();
